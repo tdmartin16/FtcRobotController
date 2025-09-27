4c8100978425e23f01272b14f28af9b9911e134b
tdmartin16 <tdmartin16@gmail.com>
2025-09-26 20:53:10 -0500

    Create HelloWorld.java
    First Tutorial

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Disabled
+@Autonomous
+public class HelloWorld extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("Hello:", "Tim");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
