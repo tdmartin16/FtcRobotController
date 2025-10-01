67448ed01d55c5b3e147e25b452d2f99ab92d4c8
tdmartin16 <tdmartin16@gmail.com>
2025-09-30 19:48:12 -0500

    RobotLocationPractice
    Create classes separate from OpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IfPractice.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Disabled
+public class IfPractice extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+        double motorSpeed = gamepad1.left_stick_y;
+
+        if (!gamepad1.a) {
+            motorSpeed *= 0.5;
+        }
+
+        telemetry.addData("Left Stick value", motorSpeed);
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UseRobotLocationOpMode.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Disabled
+public class UseRobotLocationOpMode extends OpMode {
+
+    RobotLocationPractice robotLocationPractice = new RobotLocationPractice(0);
+    
+
+    @Override
+    public void init() {
+        robotLocationPractice.setAngle(0);
+        robotLocationPractice.setX(0);
+        robotLocationPractice.setY(0);
+
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.a) {
+            robotLocationPractice.turnRobot(0.1);
+        } else if (gamepad1.b) {
+            robotLocationPractice.turnRobot(-0.1);
+        }
+
+        if (gamepad1.dpad_left) {
+            robotLocationPractice.changeX(0.1);
+        } else if (gamepad1.dpad_right) {
+            robotLocationPractice.changeX(-0.1);
+        }
+
+        if (gamepad1.dpad_down) {
+            robotLocationPractice.changeY(0.1);
+        } else if (gamepad1.dpad_up) {
+            robotLocationPractice.changeY(-0.1);
+        }
+
+        telemetry.addData("Heading", robotLocationPractice.getHeading());
+        telemetry.addData("Angle", robotLocationPractice.getAngle());
+        telemetry.addData("X Value", robotLocationPractice.getX());
+        telemetry.addData("Y Value", robotLocationPractice.getY());
+
+    }
+}
