ba2c0423018a6ab94af007f15827bb066a390131
tdmartin16 <tdmartin16@gmail.com>
2025-10-01 21:47:06 -0500

    DC Motor
    Dc Motor practice

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/TestBench.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/TestBench.java
@@ -1,15 +1,28 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class TestBench {
     private DigitalChannel touchSensor;
+    private DcMotor motor;
+    private double ticksPerRev;
 
-    public void init(HardwareMap haMap) {
-        touchSensor = haMap.get(DigitalChannel.class, "touch_sensor");
+
+    public void init(HardwareMap hwMap) {
+        touchSensor = hwMap.get(DigitalChannel.class, "touch_sensor");
         touchSensor.setMode(DigitalChannel.Mode.INPUT);
+
+        motor = hwMap.get(DcMotor.class, "motor");
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        ticksPerRev = motor.getMotorType().getTicksPerRev();
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor.setDirection(DcMotorSimple.Direction.FORWARD);
     }
+
+    /*   Touch Sensor Methods  */
 
     public boolean isTouchSensorPressed() {
         return !touchSensor.getState();
@@ -17,5 +30,19 @@ public class TestBench {
 
     public boolean isTouchSensorReleased() {
         return !touchSensor.getState();
+    }
+
+    /*  DC Motor Methods  */
+
+    public void setMotorSpeed(double speed) {
+        motor.setPower(speed);
+    }
+
+    public double getMotorRevs() {
+        return motor.getCurrentPosition() / ticksPerRev;
+    }
+
+    public void setMotorZeroBehavior(DcMotor.ZeroPowerBehavior zeroBehavior) {
+        motor.setZeroPowerBehavior(zeroBehavior);
     }
 }
