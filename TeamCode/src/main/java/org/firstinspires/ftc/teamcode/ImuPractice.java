71cedce40cff928e7c212881f93ad7e61fcb84b5
tdmartin16 <tdmartin16@gmail.com>
2025-10-04 08:04:13 -0500

    Rumble Practice
    Rumble Practice

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ImuPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ImuPractice.java
@@ -1,12 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.mechanisms.TestBenchIMU;
 
-@TeleOp
+@Disabled
 public class ImuPractice extends OpMode {
     TestBenchIMU bench = new TestBenchIMU();
     double heading;
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RumbleTest.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Disabled
+public class RumbleTest extends OpMode {
+
+    double endGameStart;
+    boolean isEndGame;
+
+    @Override
+    public void init() {
+
+    }
+
+    public void Start() {
+        endGameStart = getRuntime() + 90;
+    }
+
+    @Override
+    public void loop() {
+        if (endGameStart > = getRuntime() && !isEndGame) {
+            gamepad1.rumbleBlips(3);
+            isEndGame = true;
+        }
+
+    }
+}
