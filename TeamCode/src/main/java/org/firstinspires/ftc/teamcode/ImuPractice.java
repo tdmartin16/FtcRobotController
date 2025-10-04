91e8b72a70d3d7dbca21c7f973fa0ff75f9e93b3
tdmartin16 <tdmartin16@gmail.com>
2025-10-03 20:20:08 -0500

    IMU Practice
    IMU Practice

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ImuPractice.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.mechanisms.TestBenchIMU;
+
+@TeleOp
+public class ImuPractice extends OpMode {
+    TestBenchIMU bench = new TestBenchIMU();
+    double heading;
+
+    @Override
+    public void init() {
+        bench.init(hardwareMap);
+
+    }
+
+    @Override
+    public void loop() {
+        heading = bench.getHeading(AngleUnit.DEGREES);
+        telemetry.addData("Heading", bench.getHeading(AngleUnit.DEGREES));
+
+        if (heading < 0.5 && heading > -0.5) {
+            bench.setMotor(0.0);
+        } else if (heading > 0.5) {
+            bench.setMotor(0.1);
+        }
+        else {
+            bench.setMotor(-0.1);
+        }
+
+
+    }
+}
